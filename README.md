ddfeb4fee21790f89b6178f823ffaf6e2f852743
giedriusstankauskas <46478940+giedriusstankauskas@users.noreply.github.com>
2023-05-10 13:32:38 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# moneyClicker
- 
+# Money Clicker
+Simple clicker that was created for a scavenger hunt. 
+<br>
+JavaScript code should be obfuscated so it would be harder to get codes without clicking.
+<hr>
+<img src="images/clickerScrsht.png" alt="screenshot" width="300"/>
+
+
---
+++ app.js
@@ -0,0 +1,56 @@
+  let Ox4a9ced = 0;
+  document.body.onkeyup = function(e) {
+    if (e.key == " " ||
+        e.code == "Space" ||
+        e.keyCode == 32
+    ) {
+      Ox4a9ced++;
+      let numb = document.getElementById("vns");
+      numb.innerText = Ox4a9ced;
+      let dollar = document.getElementById("dolla");
+      dollar.style.width = "51%";
+      setTimeout(function(){
+        dollar.style.width = "50%";
+      }, 100);
+      Ox382ao7 = Ox4a9ced
+      if (Ox382ao7 > 7999) {
+        document.getElementById("five").innerText = atob('UEVOS1RBU0tPREFT');
+      } else if (Ox382ao7 > 3999) {
+        document.getElementById("four").innerText = atob('S0VUVklSVEFTS09EQVM=');
+      } else if (Ox382ao7 > 2999) {
+        document.getElementById("three").innerText = atob('VFJFQ0lBU0tPREFT');
+      } else if (Ox382ao7 > 1999) {
+        document.getElementById("two").innerText = atob('QU5UUkFTS09EQVM=');
+      } else if (Ox382ao7 > 999) {
+        document.getElementById("one").innerText = atob('Qk83MDBUUlVCQURVUklOSU5LQVM=');
+      }
+    }
+  }
+
+  var theElement = document.getElementById("dolla");
+  theElement.addEventListener("touchstart", handlerFunction, false);
+  function handlerFunction(event) {
+    Ox4a9ced++;
+    let numb = document.getElementById("vns");
+    numb.innerText = Ox4a9ced;
+    let dollar = document.getElementById("dolla");
+    dollar.style.width = "51%";
+    setTimeout(function(){
+      dollar.style.width = "50%";
+    }, 100);
+
+    Ox90ga87 = Ox4a9ced
+
+    if (Ox90ga87 > 7999) {
+      document.getElementById("five").innerText = atob('UEVOS1RBU0tPREFT');
+    } else if (Ox90ga87 > 3999) {
+      document.getElementById("four").innerText = atob('S0VUVklSVEFTS09EQVM=');
+    } else if (Ox90ga87 > 2999) {
+      document.getElementById("three").innerText = atob('VFJFQ0lBU0tPREFT');
+    } else if (Ox90ga87 > 1999) {
+      document.getElementById("two").innerText = atob('QU5UUkFTS09EQVM=');
+    } else if (Ox90ga87 > 999) {
+      document.getElementById("one").innerText = atob('UElSTUFTS09EQVM=');
+    }
+  }
+
